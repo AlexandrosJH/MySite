ad554a6e9a1d7f540653cf008394b215b8486ff1
Alexandros w.w.w <harvesterstart@outlook.com>
2018-10-24 00:41:37 -0700

    add new views to polls module

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("hellow world")
+
+def detail(request, question_id):
+    return HttpResponse('You looking at question %s.' % question_id)
+
+def results(request, question_id):
+    return HttpResponse('You looking at the result question %s.' % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('You voting on question %s.' % question_id)
